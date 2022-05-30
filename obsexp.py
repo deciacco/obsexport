f850c366a4c3e8fa81c2d8686bb0c2cbf0091ced
deciacco <ec@deciacco.com>
2022-05-29 21:27:15 -0500

    better comments

--- obsexp.py
+++ obsexp.py
@@ -10,34 +10,32 @@ dest_dir = "/home/ec/win_d/temp/infosec"
 
 dest_path = shutil.copytree(source_dir, dest_dir)
 
-with open("/home/ec/win_d/res_file.txt", 'w+') as rf:
-   for dir_name, sub_dir_list, file_list in os.walk(dest_path):
-      for sub_dir in sub_dir_list:
-         sub_dir_res_path = os.path.join(dir_name, os.path.join(sub_dir, res_dir_name))
-         if os.path.isdir(sub_dir_res_path):
-            sub_dir_list.remove(sub_dir)
-      
-      if len(file_list) > 0: #if we have files....
-         cur_res_path = os.path.join(dir_name, res_dir_name)
+for dir_name, sub_dir_list, file_list in os.walk(dest_path):
+   for sub_dir in sub_dir_list:
+      sub_dir_res_path = os.path.join(dir_name, os.path.join(sub_dir, res_dir_name))
+      if os.path.isdir(sub_dir_res_path):
+         sub_dir_list.remove(sub_dir)
+   
+   if len(file_list) > 0: #if we have files....
+      cur_res_path = os.path.join(dir_name, res_dir_name)
 
-         os.mkdir(cur_res_path)
+      #...create the new resource folder
+      os.mkdir(cur_res_path)
 
-         for file_name in file_list:
-            md_file = os.path.join(dir_name, file_name)
+      #fore each file...
+      for file_name in file_list:
+         md_file = os.path.join(dir_name, file_name)
 
-            #search each file for all image references
-            if os.path.isfile(md_file) and pathlib.PurePath(md_file).suffix == '.md':
-               rf.write(md_file + '\n')
+         #...if it's an MD file
+         if os.path.isfile(md_file) and pathlib.PurePath(md_file).suffix == '.md':
+            with open(md_file, 'r') as md_f:
+               md_text = md_f.read()
+   
+            #use regex, for each resource, copy it form the old folder
+            #use the first group (file name) form the regex search
+            for file_resource in re.compile(r"_res\/(.*)\)").finditer(md_text):
+               shutil.copy(os.path.join(source_res_dir, file_resource.group(1)), cur_res_path)
 
-               with open(md_file, 'r') as md_f:
-                  md_text = md_f.read()
-      
-               for file_resource in re.compile(r"_res\/(.*)\)").finditer(md_text):
-                  rf.write(os.path.join(source_res_dir, file_resource.group(1)) + " --> " + cur_res_path + '\n')
-                  shutil.copy(os.path.join(source_res_dir, file_resource.group(1)), cur_res_path)
-
-               with open(md_file, 'w+') as md_f:
-                  md_f.write(md_text.replace("../",""))
-      
-      
-      
+            #remove the '../' form the links in the MD files, not needed anymore
+            with open(md_file, 'w+') as md_f:
+               md_f.write(md_text.replace("../",""))  
